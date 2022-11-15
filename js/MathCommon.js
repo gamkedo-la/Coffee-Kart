eb2fa628818c16364a45a75f2570d06de262a12b
faircall <cooper.willis@gmail.com>
2022-11-15 22:22:09 +1000

    more math functionality

--- js/MathCommon.js
+++ js/MathCommon.js
@@ -10,9 +10,44 @@ function acosDeg(rad) {
     return radToDeg(Math.acos(rad));
 }
 
-function Vec2Init(xval, yval) {
-    result = {x : xval, y : yval};        
+function asinDeg(rad) {
+    return radToDeg(Math.asin(rad));
+}
+
+function cosDeg(rad) {
+    return Math.cos(rad_to_deg(rad));
+}
+
+function sinDeg(rad) {
+    return Math.sin(rad_to_deg(rad));
+}
+
+function Vec2Init(xVal, yVal) {
+    var result = {x : xVal, y : yVal};        
     return result;
+}
+
+function Vec2PolarInit(angle, length) {
+    var result = Vec2Init(length * cosDeg(angle), length * sinDeg(angle));
+    return result;
+}
+
+function Vec2Angle(v) {     
+    const length = Vec2Mag(v);
+    if (length < epsilon) {
+        return 0.0;
+    }
+    const cosTheta = v.x / length;
+    const sinTheta = v.y / length;
+    const acosTheta = acosDeg(cosTheta);
+    const asinTheta = asinDeg(sinTheta);
+
+    // 4 quadrants
+    if (asinTheta < 0.0) {
+        return 360 - acosTheta;
+    } else {
+        return acosTheta;
+    }
 }
 
 function Vec2Dot(v1, v2) {
@@ -20,12 +55,12 @@ function Vec2Dot(v1, v2) {
 }
 
 function Vec2Add(v1, v2) {
-    result = Vec2Init(v1.x + v2.x, v1.y + v2.y);
+    var result = Vec2Init(v1.x + v2.x, v1.y + v2.y);
     return result;
 }
 
 function Vec2Scale(v1, s) {
-    result = Vec2Init(v1.x*s, v1.y*s);
+    var result = Vec2Init(v1.x*s, v1.y*s);
     return result;
 }
 
@@ -36,13 +71,14 @@ function Vec2Mag(v) {
 function Vec2AngleBetween(v1, v2) {
     // check for
     // potential div-zero 
-    var v1Mag = Vec2Mag(v1);
-    var v2Mag = Vec2Mag(v2);
+    const v1Mag = Vec2Mag(v1);
+    const v2Mag = Vec2Mag(v2);
+    const epsilon = 0.0001;
     if (v1Mag < epsilon || v2Mag < epsilon) {
         return 0.0;
     }
-    var cosTheta = Vec2Dot(v1, v2) / (Vec2Mag(v1) * Vec2Mag(v2));
-    var theta = acosDeg(cosTheta);
+    const cosTheta = Vec2Dot(v1, v2) / (Vec2Mag(v1) * Vec2Mag(v2));
+    const theta = acosDeg(cosTheta);
     return theta;
 }
 
