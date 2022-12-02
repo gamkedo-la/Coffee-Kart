c6d795ac03cecb3c3b4b55083e1cd6ef4e5a5dd1
faircall <cooper.willis@gmail.com>
2022-12-03 01:30:26 +1000

    create waypoint class

---
+++ js/Waypoint.js
@@ -0,0 +1,8 @@
+function WaypointClass() {
+    this.initWaypoint = function(xPos, yPos, angle, width) {
+        this.x = xPos;
+        this.y = yPos;
+        this.angle = angle;
+        this.width = width;
+    }
+}
