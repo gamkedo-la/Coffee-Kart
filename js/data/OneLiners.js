ba0b6e9ed0624cc7bceb19aab8bcf671b8405065
Jared Rigby <talk@jaredrigby.co.uk>
2023-01-29 14:40:30 +0800

    WIP thinking about how to create a one-liner system

---
+++ js/data/OneLiners.js
@@ -0,0 +1,27 @@
+const oneLiners = [
+    {
+        text: "Cool beans - you're in first place!",
+        event: "racePosition",
+        value: 1
+    },
+    {
+        text: "I'll get first place by whatever beans necessary!",
+        event: "racePosition",
+        value: 2
+    },
+    {
+        text: "It's espresso time!",
+        event: "racePosition",
+        value: 3
+    },
+    {
+        text: "Don't be latte, you're coming last-ay!",
+        event: "racePosition",
+        value: 4
+    },
+    {
+        text: "This tastes like mud!",
+        event: "offTrack",
+        value: true
+    }
+];
