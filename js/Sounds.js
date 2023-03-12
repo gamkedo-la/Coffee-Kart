2d4000d6b33f812e4d42a56e7e9ec333e7f0eef7
faircall <cooper.willis@gmail.com>
2023-03-12 13:32:22 +1000

    fixed music bug

--- js/Sounds.js
+++ js/Sounds.js
@@ -6,53 +6,52 @@ countdown_beep.volume = 0.5;
 var countdown_start = new Audio('sounds/countdown_start.mp3');
 countdown_start.volume = 0.5;
 
-var snowMusic = new Audio('sounds/snowMusic-Coffeekart.mp3');
-snowMusic.volume = 0.2;
-snowMusic.loop = true;
 
-var cafeMusic = new Audio('sounds/coffeeKart-rock.mp3');
-cafeMusic.volume = 0.2;
-cafeMusic.loop = true;
+const MUSIC_VOLUME = 0.2;
 
-var jungleMusic = new Audio('sounds/coffeeKart-add-guitarHarmonics.mp3');
-jungleMusic.volume = 0.2;
-jungleMusic.loop = true;
+var jukeBoxCurrent = null;
 
 function switchSong(songName) {
     if (jukeBox != null) {
         jukeBox.pause();
     }
-    if (songName == "snowMusic") {
-        jukeBox = snowMusic;        
-    }
-    if (songName == "cafeMusic") 
-    {
-        jukeBox = cafeMusic;
-    }
-    if (songName == "jungleMusic") 
-    {
-        jukeBox = jungleMusic;
-    }
+    jukeBox = new Audio('sounds/' + songName + '.mp3');
+    jukeBox.loop = true;
+    jukeBox.volume = MUSIC_VOLUME;
     jukeBox.play();
+
+    
 }
 
 function updateSounds() {
     if (gGameState == GS_RACING) {
         if (courseIndex == SNOW_LEVEL) {
-            if (jukeBox != snowMusic) {
-                switchSong("snowMusic");                
+            if (jukeBoxCurrent != "snowMusic") {
+                jukeBoxCurrent = "snowMusic";
+                switchSong("snowMusic");
             }
         }
         if (courseIndex == CAFE_LEVEL) {
-            if (jukeBox != cafeMusic) {
-                switchSong("cafeMusic");                
+            if (jukeBoxCurrent != "cafeMusic") {
+                jukeBoxCurrent = "cafeMusic";
+                switchSong("cafeMusic");
             }
+
+            
         }
 
         if (courseIndex == JUNGLE_LEVEL) {
-            if (jukeBox != jungleMusic) {
-                switchSong("jungleMusic");                
+            if (jukeBoxCurrent != "jungleMusic") {
+                jukeBoxCurrent = "jungleMusic";
+                switchSong("jungleMusic");
             }
+            
+        
         }
-    }
+    } else { 
+        if (jukeBoxCurrent != "menuMusic") {
+            jukeBoxCurrent = "menuMusic";
+            switchSong("menuMusic");
+        }         
+     }
 }
